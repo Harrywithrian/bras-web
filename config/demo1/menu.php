0b86267ddf5b391fe03d6fe73600526e9eefbbd5
Hari Andrian <hry.andrian@gmail.com>
2021-10-03 15:56:46 +0700

    init
    push aplikasi awal

---
+++ config/demo1/menu.php
@@ -0,0 +1,137 @@
+<?php
+
+use App\Core\Adapters\Theme;
+
+return [
+    // Refer to config/global/menu.php
+
+    // Main Menu
+    'main' => [
+        [
+            'title' => 'Home',
+            'icon' => [
+                'font' => '<i class="bi bi-house fs-2"></i>',
+            ]
+        ],
+        [
+            'title' => 'Admin Panel',
+            'icon' => [
+                'font' => '<i class="bi bi-folder fs-2"></i>',
+            ],
+            'classes' => ['item' => 'menu-accordion'],
+            'attributes' => [
+                "data-kt-menu-trigger" => "click",
+            ],
+            'sub' => [
+                'class' => 'menu-sub-accordion menu-active-bg',
+                'items' => [
+                    [
+                        'title'  => 'Manajemen User',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                    [
+                        'title'  => 'Menu',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                    [
+                        'title'  => 'Role',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                    [
+                        'title'  => 'Permission',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                ]
+            ]
+        ],
+        [
+            'title' => 'Master Data',
+            'icon' => [
+                'font' => '<i class="bi bi-file-earmark fs-2"></i>',
+            ],
+            'classes' => ['item' => 'menu-accordion'],
+            'attributes' => [
+                "data-kt-menu-trigger" => "click",
+            ],
+            'sub' => [
+                'class' => 'menu-sub-accordion menu-active-bg',
+                'items' => [
+                    [
+                        'title'  => 'Wasit',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                ]
+            ]
+        ],
+        [
+            'title' => 'Penugasan',
+            'icon' => [
+                'font' => '<i class="bi bi-calendar2-check fs-2"></i>',
+            ],
+            'classes' => ['item' => 'menu-accordion'],
+            'attributes' => [
+                "data-kt-menu-trigger" => "click",
+            ],
+            'sub' => [
+                'class' => 'menu-sub-accordion menu-active-bg',
+                'items' => [
+                    [
+                        'title'  => 'Pengajuan',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                    [
+                        'title'  => 'Approval',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                    [
+                        'title'  => 'List Tugas',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                ]
+            ]
+        ],
+        [
+            'title' => 'Pertandingan',
+            'icon' => [
+                'font' => '<i class="bi bi-layout-text-sidebar-reverse fs-2"></i>',
+            ]
+        ],
+        [
+            'title' => 'Report',
+            'icon' => [
+                'font' => '<i class="bi bi-pie-chart fs-2"></i>',
+            ],
+            'classes' => ['item' => 'menu-accordion'],
+            'attributes' => [
+                "data-kt-menu-trigger" => "click",
+            ],
+            'sub' => [
+                'class' => 'menu-sub-accordion menu-active-bg',
+                'items' => [
+                    [
+                        'title'  => 'Evaluasi Wasit',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                    [
+                        'title'  => 'Evaluasi Pertandingan',
+                        'path'   => '#',
+                        'bullet' => '<span class="bullet bullet-dot"></span>',
+                    ],
+                ]
+            ]
+        ],
+    ],
+
+    // Horizontal menu
+    'horizontal' => [
+    ],
+];
