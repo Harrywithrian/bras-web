34a5c11551db337af02f92102acaf76d256007cd
Hari Andrian <hry.andrian@gmail.com>
2022-11-01 00:36:07 +0700

    ketinggalan

--- resources/views/master/user/show.blade.php
+++ resources/views/master/user/show.blade.php
@@ -15,7 +15,7 @@
 
     <ol class="breadcrumb text-muted fs-6 fw-bold mb-5">
         <li class="breadcrumb-item pe-3"><a href="{{ route('index') }}" class="pe-3"><i class="bi bi-house-door" style="margin-bottom:5px;"></i> Home</a></li>
-        <li class="breadcrumb-item pe-3"><a href="{{ route('m-user.index') }}" class="pe-3">User</a></li>
+        <li class="breadcrumb-item pe-3"><a href="{{ route('m-user.index') }}" class="pe-3">Manajemen User</a></li>
         <li class="breadcrumb-item px-3 text-muted">{{ $title }}</li>
     </ol>
 
